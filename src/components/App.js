2da4645eb50e8bdd95e76b3fb07b34a725c198f0
Mehedi Haque <kpopdw@gmail.com>
2020-06-14 18:09:23 -0400

    start

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App-main">
+        <h1>App class</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
